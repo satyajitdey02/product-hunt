b75aba60ca51176b8edf3097f7d85302307e7d06
satyajitdey02 <satyajit02@gmail.com>
2019-04-05 19:07:40 +0600

    Add search result markups

--- src/App.js
+++ src/App.js
@@ -17,6 +17,123 @@ class App extends Component {
             </div>
           </div>
 
+          <div className="row search-results">
+            <ul className="col result-list">
+              <li className="result-item">
+
+                <a href="/" className="product-link">
+
+                  <div className="thumbnail">
+                    <img src="https://ph-files.imgix.net/fba5b9a4-0d3c-4806-ba4b-10f27398c60d?auto=format&auto=compress&codec=mozjpeg&cs=strip&w=80&h=80&fit=crop&dpr=2" alt="Prod-1"/>
+                  </div>
+                  <div className="product-info">
+                    <div className="vote">
+                      &#x25B2;
+                      <span> 4</span>
+                    </div>
+                    <h3 className="title">
+                      Product Mindset
+                    </h3>
+                    <p className="leadtext">Free book on how to get inside
+                      your customer’s mind</p>
+
+                    <div className="credit">
+                      <span className="submitted">Submitted By: </span>
+                      <img
+                          src="https://abs.twimg.com/sticky/default_profile_images/default_profile_4_normal.png"
+                          alt="Submitted By"/>
+                    </div>
+
+                  </div>
+                </a>
+              </li>
+              <li className="result-item">
+
+                <a href="/" className="product-link">
+
+                  <div className="thumbnail">
+                    <img src="https://ph-files.imgix.net/fba5b9a4-0d3c-4806-ba4b-10f27398c60d?auto=format&auto=compress&codec=mozjpeg&cs=strip&w=80&h=80&fit=crop&dpr=2" alt="Prod-2"/>
+                  </div>
+                  <div className="product-info">
+                    <div className="vote">
+                      &#x25B2;
+                      <span> 3</span>
+                    </div>
+                    <h3 className="title">
+                      Product Mindset
+                    </h3>
+                    <p className="leadtext">Free book on how to get inside
+                      your customer’s mind</p>
+
+                    <div className="credit">
+                      <span className="submitted">Submitted By: </span>
+                      <img
+                          src="https://abs.twimg.com/sticky/default_profile_images/default_profile_4_normal.png"
+                          alt="Submitted By"/>
+                    </div>
+
+                  </div>
+                </a>
+              </li>
+              <li className="result-item">
+
+                <a href="/" className="product-link">
+
+                  <div className="thumbnail">
+                    <img src="https://ph-files.imgix.net/fba5b9a4-0d3c-4806-ba4b-10f27398c60d?auto=format&auto=compress&codec=mozjpeg&cs=strip&w=80&h=80&fit=crop&dpr=2" alt="Prod-3"/>
+                  </div>
+                  <div className="product-info">
+                    <div className="vote">
+                      &#x25B2;
+                      <span> 2</span>
+                    </div>
+                    <h3 className="title">
+                      Product Mindset
+                    </h3>
+                    <p className="leadtext">Free book on how to get inside
+                      your customer’s mind</p>
+
+                    <div className="credit">
+                      <span className="submitted">Submitted By: </span>
+                      <img
+                          src="https://abs.twimg.com/sticky/default_profile_images/default_profile_4_normal.png"
+                          alt="Submitted By"/>
+                    </div>
+
+                  </div>
+                </a>
+              </li>
+              <li className="result-item">
+
+                <a href="/" className="product-link">
+
+                  <div className="thumbnail">
+                    <img src="https://ph-files.imgix.net/fba5b9a4-0d3c-4806-ba4b-10f27398c60d?auto=format&auto=compress&codec=mozjpeg&cs=strip&w=80&h=80&fit=crop&dpr=2" alt="Prod-4"/>
+                  </div>
+                  <div className="product-info">
+                    <div className="vote">
+                      &#x25B2;
+                      <span> 1</span>
+                    </div>
+                    <h3 className="title">
+                      Product Mindset
+                    </h3>
+                    <p className="leadtext">Free book on how to get inside
+                      your customer’s mind</p>
+
+                    <div className="credit">
+                      <span className="submitted">Submitted By: </span>
+                      <img
+                          src="https://abs.twimg.com/sticky/default_profile_images/default_profile_4_normal.png"
+                          alt="Submitted By"/>
+                    </div>
+
+                  </div>
+                </a>
+              </li>
+            </ul>
+          </div>
+
         </div>
     );
   }
