47d02b64ec928c29ce63d94fd16a56e093e47a3e
satyajitdey02 <satyajit02@gmail.com>
2019-04-09 17:18:04 +0600

    Remove console.log from MessageBox

--- src/components/MessageBox.js
+++ src/components/MessageBox.js
@@ -1,12 +1,9 @@
 import React from 'react';
 
 const MessageBox = ({message}) => {
-
-  console.log('Inside Message Box!')
   return (
       <p className="message-box">{message}</p>
   );
-
 };
 
 export default MessageBox;
