0d61c80ca157af82f8f6e665faf83de144f720f1
satyajitdey02 <satyajit02@gmail.com>
2019-04-17 12:03:38 +0600

    Define propTypes of Byline Component

--- src/components/ByLine.js
+++ src/components/ByLine.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 const ByLine = ({credit}) => {
 
@@ -12,6 +13,10 @@ const ByLine = ({credit}) => {
 
   );
 
+};
+
+ByLine.propTypes = {
+  credit: PropTypes.object
 };
 
 export default ByLine;
