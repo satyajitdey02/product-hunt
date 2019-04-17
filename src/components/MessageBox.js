ae60bb2ecb719f37b8b28850e02e64fc810444c6
satyajitdey02 <satyajit02@gmail.com>
2019-04-17 12:03:58 +0600

    Define propTypes of MessageBox Component

--- src/components/MessageBox.js
+++ src/components/MessageBox.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 const MessageBox = ({message}) => {
   return (
       <p className="message-box">{message}</p>
   );
+};
+
+MessageBox.propTypes = {
+  message: PropTypes.string
 };
 
 export default MessageBox;
