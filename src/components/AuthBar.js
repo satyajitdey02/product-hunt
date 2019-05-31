7cc8e4a3962b05ec6e93ad517eccdd576c0959b9
satyajitdey02 <satyajit02@gmail.com>
2019-05-31 16:52:42 +0600

    Add little Style to auth bar

--- src/components/AuthBar.js
+++ src/components/AuthBar.js
@@ -3,7 +3,7 @@ import React from 'react';
 const AuthBar = (props) => {
   return (
       <div className="col-md-2 auth-bar">
-        <span><a href="#">LOGIN</a> | <a href="#">LOGOUT</a></span>
+        <span><a href="#">Login</a> | <a href="#">Logout</a></span>
       </div>
   );
 };
